0ba9ac58b27f74bf9635420ff335e02803e1c1be
udaykalyan97 <57364428+udaykalyan97@users.noreply.github.com>
2020-12-11 18:38:51 -0500

    Create assignment4.go

---
+++ assignment4.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const (
+	x = 6
+	y
+	z
+)
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+
+/*-------------------------------
+OUTPUT: x=6, y=6, z=6
+---------------------------------*/
